8fad78fad8e9c6d58a396340ca21c7e3f15c0f20
Stuart Winchester <stuart.winchester@gmail.com>
2017-04-20 15:55:36 +0100

    new shell scripts

---
+++ generate-fail-run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+randpw(){ < /dev/urandom tr -dc a-z-0-9 | head -c${1:-8};echo;}
+
+podname=$( randpw )
+
+date; oc run -i $podname --image=172.30.1.1:5000/myproject/simple-ocp-docker:latest --restart=Never --command -- /bin/bash -c "/tmp/false.sh"; oc get pods $podname -o json | jq '.status.phase' | grep "Succeeded" ; echo $? ; date; oc delete pods $podname
